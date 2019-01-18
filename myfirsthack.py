9ce29dc68efab3a460eb3c4ee1b134c75ded5636
Ujwal kumar <ukujwalkumar80@gmail.com>
2019-01-18 04:35:49 -0500

    Add files via upload

---
+++ myfirsthack.py
@@ -0,0 +1,37 @@
+import optparse
+import zipfile
+from threading import Thread
+
+def extract_zip(zfile, password):
+    try:
+        zfile.extractall(pwd=password)
+        print ("[+] Password Found: " + password + '\n')
+        exit(0)
+    except:
+        pass
+
+def main():
+    parser = optparse.OptionParser("usage %prog "+\
+                                   "-f <zipfile> -d <dicctionary>")
+    parser.add_option('-f', dest='zname', type='string',\
+                      help='specify zip file')
+    parser.add_option('-d', dest='dname', type='string',\
+                      help='specify dictionary file')
+    (options, arg) = parser.parse_args()
+    if (options.zname == None) | (options.dname == None):
+        print (parser.usage)
+        exit(0)
+    else:
+        zname = options.zname
+        dname = options.dname
+
+    zFile = zipfile.ZipFile(zname)
+    passFile = open(dname)
+
+    for line in passFile.readlines():
+        password = line.strip('\n')
+        t = Thread(target=extract_zip, args=(zFile, password))
+        t.start()
+
+if __name__ == '__main__':
+    main()
